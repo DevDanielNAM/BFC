d1ddb433fef8df66cc210017e00d64062852e257
DevDanielNAM <danielnam426@gmail.com>
2024-06-09 05:45:07 +0900

    [modify] #3: 댓글 개별 삭제 처리위한 코드 수정

--- src/main/webapp/board/replyList.jsp
+++ src/main/webapp/board/replyList.jsp
@@ -3,6 +3,9 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" href="../resources/css/boardDetail.css">
+</head>
 	<%
         ReplyDAO replyDAO = new ReplyDAO();
         List<ReplyDTO> replyList = replyDAO.getAllReplies();
@@ -10,7 +13,8 @@
 
     <ul class="reply-view-lists">
      	<%
-            for (ReplyDTO reply : replyList) {
+     	for (int i = 0; i < replyList.size(); i++) {
+            ReplyDTO reply = replyList.get(i);
         %>
 		<li class="reply-view-list">
 			<section class="reply-view">
@@ -18,15 +22,14 @@
 					<img id="reply-profile-image" src="../resources/images/profile.png" alt="reply-profile" width="41" height="41">
 					<section class="reply-profile-contents-wrap">
 						<article class="reply-profile-contents">
-						<!-- DB에서 가져올 예정 -->
 							<h3 id="reply-profile-nickname"><%= reply.getWriterNickname() %></h3>
 							<h6 id="reply-profile-date"><%= reply.getCreatedAt().toString().substring(0, 19) %></h6>
 						</article>
 						<article class="reply-view-more-button">
-							<img id="reply-more-button" src="../resources/images/kebab_menu_icon.png" alt="reply more button" width="19" height="19" />									
-							<ul id="reply-view-more-button-lists">
-								<li>수정</li>
-								<li>삭제</li>
+							<img id="reply-more-button-<%= i %>" class="reply-more-button" src="../resources/images/kebab_menu_icon.png" alt="reply more button" width="19" height="19" />									
+							<ul id="reply-view-more-button-lists-<%= i %>" class="reply-view-more-button-lists">
+								<li><a onclick="return confirm('댓글을 수정하시겠습니까?');">수정</a></li>
+								<li><a href="deleteReply.jsp?replyId=<%= reply.getReplyId() %>" onclick="return confirm('댓글을 삭제하시겠습니까?');">삭제</a></li>
 							</ul>
 						</article>
 					</section>
@@ -43,4 +46,5 @@
             }
         %>
 	</ul>
+	<script type="text/javascript" src="../resources/js/boardDetail.js" defer></script>
 </html>
--- src/main/webapp/resources/js/boardDetail.js
+++ src/main/webapp/resources/js/boardDetail.js
@@ -21,33 +21,40 @@ const confirmSubmission = () => {
 
 // reply more button 클릭 시 수정, 삭제 toggle
 document.addEventListener("DOMContentLoaded", () => {
-    const moreButton = document.getElementById("reply-more-button");
-    const moreButtonLists = document.getElementById("reply-view-more-button-lists");
+    const moreButtons = document.querySelectorAll(".reply-more-button");
+    const moreButtonLists = document.querySelectorAll(".reply-view-more-button-lists");
 
-    moreButton.onclick = (event) => {
-        event.stopPropagation(); // 이벤트 전파를 막음
-        if (moreButtonLists.classList.contains("show")) {
-            moreButtonLists.classList.remove("show");
-            setTimeout(() => {
-                moreButtonLists.style.display = "none";
-            }, 300); // transition 시간과 동일
-        } else {
-            moreButtonLists.style.display = "block";
-            setTimeout(() => {
-                moreButtonLists.classList.add("show");
-            }, 10); // 약간의 지연을 주어 display 변경 후 transition이 적용되도록 함
-        }
-    };
+    moreButtons.forEach((moreButton, index) => {
+        const moreButtonList = document.getElementById(`reply-view-more-button-lists-${index}`);
+
+        moreButton.onclick = (event) => {
+            event.stopPropagation(); // 이벤트 전파를 막음
+            if (moreButtonList.classList.contains("show")) {
+                moreButtonList.classList.remove("show");
+                setTimeout(() => {
+                    moreButtonList.style.display = "none";
+                }, 300); // transition 시간과 동일
+            } else {
+                moreButtonList.style.display = "block";
+                setTimeout(() => {
+                    moreButtonList.classList.add("show");
+                }, 10); // 약간의 지연을 주어 display 변경 후 transition이 적용되도록 함
+            }
+        };
+    });
 
     document.addEventListener("click", (event) => {
-        if (event.target !== moreButton && !moreButtonLists.contains(event.target)) {
-            if (moreButtonLists.classList.contains("show")) {
-                moreButtonLists.classList.remove("show");
-                setTimeout(() => {
-                    moreButtonLists.style.display = "none";
-                }, 300); // transition 시간과 동일
+        moreButtonLists.forEach((moreButtonList, index) => {
+            const moreButton = document.getElementById(`reply-more-button-${index}`);
+            if (event.target !== moreButton && !moreButtonList.contains(event.target)) {
+                if (moreButtonList.classList.contains("show")) {
+                    moreButtonList.classList.remove("show");
+                    setTimeout(() => {
+                        moreButtonList.style.display = "none";
+                    }, 300); // transition 시간과 동일
+                }
             }
-        }
+        });
     });
 });
 
